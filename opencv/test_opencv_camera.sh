cc3e8a439b0036ba52c665fe7b6a4c25f5c29c4a
Caesar Wang <wxt@rock-chips.com>
2019-01-12 14:09:07 +0800

    opencv: add python3.7
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- opencv/test_opencv_camera.sh
+++ opencv/test_opencv_camera.sh
@@ -4,5 +4,6 @@
 export DISPLAY=:0.0 
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
-su linaro -c "python test.py"
+#su linaro -c "python /usr/local/bin/test.py"
+su linaro -c "python3.7 /usr/local/bin/test.py"
 
